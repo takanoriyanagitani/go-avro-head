c89afc3bd43dacea797c56836cf352787fb4bfc5
yanagitani <takanori.yanagitani@gmail.com>
2024-12-10 07:29:01 +0900

    feat: input config(max blob size)

--- head.go
+++ head.go
@@ -12,3 +12,21 @@ type Input struct {
 	Rows iter.Seq2[AvroRow, error]
 	AvroSchemaJson
 }
+
+const (
+	BlobSizeMaxDefault int = 1048576
+)
+
+type InputConfig struct {
+	maxBlobSize int
+}
+
+func (c InputConfig) WithBlobSizeMax(i int) InputConfig {
+	c.maxBlobSize = i
+	return c
+}
+
+func (c InputConfig) BlobSizeMax() int { return c.maxBlobSize }
+
+var InputConfigDefault InputConfig = InputConfig{}.
+	WithBlobSizeMax(BlobSizeMaxDefault)
